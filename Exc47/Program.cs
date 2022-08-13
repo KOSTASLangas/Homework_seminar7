609c2c1360d836b136eab0f89eacde885085c7ee
Kostas <langas.kostya@mail.ru>
2022-08-13 19:08:29 +0500

    Двумерный массив с дробными(вещественными числами)

---
+++ Exc47/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToDouble(new Random().Next(-100,100) / 10.0);  
+}
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
