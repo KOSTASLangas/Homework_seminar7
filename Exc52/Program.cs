bbdc01c5c6c98c334eb6c4daa0a1ed87acd1c6a5
Kostas <langas.kostya@mail.ru>
2022-08-13 20:35:51 +0500

    Двумерный массив среднее арифметическое по столбикам

---
+++ Exc52/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,11));  
+}
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+
+int b = 0;
+
+while (b < n)
+{
+    double Count = 0;
+    int a = 0;
+    while (a < m)
+    
+    {
+    Count = (Count + array[a, b]);
+    a++;
+    }
+    Count = Count / a;
+    Console.Write(Count + "; ");
+    b++;
+}
+
+
